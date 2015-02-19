88ecfa11d3a2aa762dbeab209ff6654fa206407b
thom2026 <thomas.b.n@hotmail.com>
2015-02-19 10:35:18 +0100

    klasser

---
+++ student_helper/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_helper
+{
+    class Event
+    {
+        public DateTime EndDate { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
